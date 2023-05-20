eba8ecc68af3f1321c8dfce358547060f9643da2
Abdullah Al-harbi <129707508+Abdullahsaud1@users.noreply.github.com>
2023-05-20 03:19:34 +0300

    The first day

---
+++ day1.py
@@ -0,0 +1,57 @@
+# name = "Abdullah"
+# age = 25
+# print(age + name)
+# i = 1
+# My_name =input("Emter your name : ")
+# print("My name is  " + My_name)
+# u_age=input("How old are you ?")
+# print("Yor are " + u_age + " Years Old!")
+# print(type(name)) #يطلع لك نوع المتغير
+#-----------------------------------------
+# names = ["Abdullah ", "Saud" , "Gazy" , "Al Hrbiy" ,1 ,2 , True] #List
+# names[0] = 'abdullah' #تغير عنصر من مصفوفه
+# print(names)
+# names.append("Abdullah") #اضافة في الداله
+# names.remove("Saud") #الحذف من الداله
+# print(names) 
+#-----------------------------------------------------
+# name_Studnt ={"name ": "Abdullah" , "how old yaers " : "12" , "Where from " : "Hail "}
+# print(name_Studnt.keys()) # يطبع kay 
+# print(name_Studnt.values()) # يطبع values الاجابه
+#------------------------------------------------------
+# if age > 18:
+#     print("Wlcoom") 
+# else :
+#     print("I am sorry ")
+#-------------------------------------------------------
+# while i <6:
+#     print(i)
+#     i+=1
+#------------------------------------------------------
+# for n in names:
+#     print(n)
+#------------------------------------------------------   
+# for r in range(6):
+#     print(r)
+#------------------------------------------------------
+# def Student1(): # الداله في البايثون مثل Motheed في الجافا و تحمل اسم 
+#     neme_Student1 = input("Enter your nume :")
+#     age_Student1 = input("Enterr your age :")
+# Student1()   #عشان نطلب الداله لازم نستعدي الداله
+# Student1()
+#------------------------------------------------------
+# def r():
+#     for r1 in range(1 , 5):
+#         print(r1)
+# r()        
+# r()
+# r()    
+#------------------------------------------------------
+# def add(num1 ,num2 ):
+#     print(num1+num2)
+# add(3 ,5)    
+# add(6 , 4)
+#------------------------------------------------------
+# def text(text1 , text2):
+#     print(text1 + text2)
+# text("I Love " , "Python")    
